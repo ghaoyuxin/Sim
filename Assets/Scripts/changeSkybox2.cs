caefb35c63655294c1588916a4ad53ade7452bea
ghaoyuxin <ghaoyuxin@163.com>
2018-11-04 17:33:38 -0500

    Changed music to 3D only at winter terrain. Added skybox change to Cave.

---
+++ Assets/Scripts/changeSkybox2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeSkybox2 : MonoBehaviour {
+
+
+		//public Material SkyboxOne;
+		public Material SkyboxThree;
+	
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.gameObject.CompareTag("Player"))
+			{
+				RenderSettings.skybox = SkyboxThree;
+				//new music
+			}
+		}
+	}
