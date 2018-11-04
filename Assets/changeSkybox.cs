5ef6603c431818b51f24f1d79772fc74ca7a7628
ghaoyuxin <ghaoyuxin@163.com>
2018-11-04 12:52:48 -0500

    Added depth of field in bw_terrian. Added change skybox between bw and grassland.

---
+++ Assets/changeSkybox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using UnityEngine;
+
+public class changeSkybox : MonoBehaviour
+{
+
+	//public Material SkyboxOne;
+	public Material SkyboxTwo;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			RenderSettings.skybox = SkyboxTwo;
+			//new music
+		}
+	}
+}
